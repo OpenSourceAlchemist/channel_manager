205bb54619569d25589d4516eeaa4a2cf2a3c9f0
Kevin Berry <kevin@opensourcealchemist.com>
2008-12-28 15:21:48 -0600

    Correct table name declaration.

--- model/user_hostmask.rb
+++ model/user_hostmask.rb
@@ -1,6 +1,5 @@
 # provides the link between a user and its hostmask's
-class UserHostmask < Sequel::Model
-  set_table_name "users_hostmasks"
+class UserHostmask < Sequel::Model(:users_hostmasks)
   many_to_one :hostmask
   many_to_one :user
 end
