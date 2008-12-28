122b10cc3f5a860502b2907b62f486a829368817
Kevin Berry <kevin@opensourcealchemist.com>
2008-12-28 12:30:26 -0600

    Add ignores for log files, adjust migrations for Sequel world, and adjust ramaze model init to get the Sequel DB running.

--- db/migrate/001_create_tables.rb
+++ db/migrate/001_create_tables.rb
@@ -1,90 +1,108 @@
 class CreateTables < Sequel::Migration
   # Create the initial tables for the database
-  def self.up
-    create_table :bans do |t| # {{{ 
-      t.column :hostmask_id, :integer, :null => false
-      t.column :created_by, :integer
-      t.column :channel_id, :integer # If no channel and no server, we'll assume its global
-      t.column :server_id, :integer # If no server and no channel, we'll assume its global
-      t.column :created_at, :datetime, :null => false
+  def up
+    create_table :bans do # {{{ 
+      primary_key :id
+      column :hostmask_id, :integer, :null => false
+      column :created_by, :integer
+      column :channel_id, :integer # If no channel and no server, we'll assume its global
+      column :server_id, :integer # If no server and no channel, we'll assume its global
+      column :created_at, :timestamp, :null => false, :default => "now()"
+      column :updated_at, :timestamp, :null => false, :default => "now()"
     end unless DB.table_exists?(:bans) # }}}
 
-    create_table :channels do |t| # {{{
-      t.column :created_by, :integer
-      t.column :updated_by, :integer
-      t.column :flags, :string
-      t.timestamps
+    create_table :channels do # {{{
+      column :created_by, :integer
+      column :updated_by, :integer
+      column :flags, :string
+      column :created_at, :timestamp, :null => false, :default => "now()"
+      column :updated_at, :timestamp, :null => false, :default => "now()"
     end unless DB.table_exists?(:channels) # }}}
 
-    create_table :consoles do |t| # {{{
-      t.timestamps
-      t.column :channel_id, :integer
-      t.column :user_id, :integer, :null => false
-      t.column :flags, :string
+    create_table :consoles do # {{{
+      column :channel_id, :integer
+      column :user_id, :integer, :null => false
+      column :flags, :string
+      column :created_at, :timestamp, :null => false, :default => "now()"
+      column :updated_at, :timestamp, :null => false, :default => "now()"
     end unless DB.table_exists?(:consoles) # }}}
 
-    create_table :exemptions do |t| # {{{
-      t.column :hostmask_id, :integer, :null => false
-      t.column :created_at, :datetime, :null => false
-      t.column :created_by, :integer
-      t.column :channel_id, :integer # If no channel, we'll assume its global
+    create_table :exemptions do # {{{
+      column :hostmask_id, :integer, :null => false
+      column :created_at, :timestamp, :null => false, :default => "now()"
+      column :updated_at, :timestamp, :null => false, :default => "now()"
+      column :created_by, :integer
+      column :channel_id, :integer # If no channel, we'll assume its global
     end unless DB.table_exists?(:exemptions) # }}}
 
-    create_table :hostmasks do |t| # {{{
-      t.column :created_at, :datetime, :null => false
-      t.column :created_by, :integer
-      t.column :mask, :string, :null => false
+    create_table :hostmasks do # {{{
+      column :created_at, :timestamp, :null => false, :default => "now()"
+      column :updated_at, :timestamp, :null => false, :default => "now()"
+      column :created_by, :integer
+      column :mask, :string, :null => false
     end unless DB.table_exists?(:hostmasks) # }}}
 
-    create_table :ignores do |t| # {{{
-      t.column :hostmask_id, :integer, :null => false
-      t.column :created_at, :datetime, :null => false
-      t.column :created_by, :integer
-      t.column :channel_id, :integer # If no channel and no server, we'll assume its global
-      t.column :server_id, :integer # If no server and no channel, we'll assume its global
+    create_table :ignores do # {{{
+      column :hostmask_id, :integer, :null => false
+      column :created_at, :timestamp, :null => false, :default => "now()"
+      column :updated_at, :timestamp, :null => false, :default => "now()"
+      column :created_by, :integer
+      column :channel_id, :integer # If no channel and no server, we'll assume its global
+      column :server_id, :integer # If no server and no channel, we'll assume its global
     end unless DB.table_exists?(:ignores) # }}}
 
-    create_table :networks do |t| # {{{
-      t.column :created_at, :datetime, :null => false
-      t.column :created_by, :integer
-      t.column :name, :string
-      t.column :description, :string
+    create_table :levels do # {{{
+      column :user_id, :integer, :nil => false
+      column :channel_id, :integer, :nil => false
+      column :level, :integer, {:nil => false, :default => 0}
+      column :flags, :string # Holdover from eggdrop users, might get imported
+      column :created_at, :timestamp, :null => false, :default => "now()"
+      column :updated_at, :timestamp, :null => false, :default => "now()"
+    end unless DB.table_exists?(:levels) # }}}
+
+    create_table :networks do # {{{
+      column :created_at, :timestamp, :null => false, :default => "now()"
+      column :updated_at, :timestamp, :null => false, :default => "now()"
+      column :created_by, :integer
+      column :name, :string
+      column :description, :string
     end unless DB.table_exists?(:networks) # }}}
 
-    create_table :servers do |t| # {{{
-      t.column :created_at, :datetime, :null => false
-      t.column :created_by, :integer
-      t.column :hostname, :string, :null => false
-      t.column :port, :integer, :null => false
-      t.column :network_id, :integer
+    create_table :servers do # {{{
+      column :created_at, :timestamp, :null => false, :default => "now()"
+      column :updated_at, :timestamp, :null => false, :default => "now()"
+      column :created_by, :integer
+      column :hostname, :string, :null => false
+      column :port, :integer, :null => false
+      column :network_id, :integer
     end unless DB.table_exists?(:servers) # }}}
 
-    create_table :users do |t| # {{{
-      t.column :authorized, :boolean, :null => false, :default => true
-      t.column :bot_flags, :string
-      t.column :comment, :string
-      t.column :created_at, :datetime, :null => false
-      t.column :created_by, :integer
-      t.column :flags, :string
-      t.column :info_line, :string
-      t.column :last_seen_at, :datetime
-      t.column :last_seen_on, :integer # Last place we saw the user
-      t.column :nick, :string
-      t.column :password, :string
-      t.column :updated_by, :integer
-      t.column :updated_at, :datetime, :null => false
+    create_table :users do # {{{
+      column :authorized, :boolean, :null => false, :default => true
+      column :bot_flags, :string
+      column :comment, :string
+      column :created_at, :timestamp, :null => false, :default => "now()"
+      column :created_by, :integer
+      column :flags, :string
+      column :info_line, :string
+      column :last_seen_at, :timestamp
+      column :last_seen_on, :integer # Last place we saw the user
+      column :nick, :string
+      column :password, :string
+      column :updated_by, :integer
+      column :updated_at, :timestamp, :null => false, :default => "now()"
     end unless DB.table_exists?(:users) # }}}
 
-    create_table :users_hostmasks do |t| # {{{
-      t.column :created_at, :datetime, :null => false
-      t.column :created_by, :integer
-      t.column :hostmask_id, :integer, :null => false
-      t.column :user_id, :integer, :null => false
+    create_table :users_hostmasks do # {{{
+      column :created_at, :timestamp, :null => false, :default => "now()"
+      column :created_by, :integer
+      column :hostmask_id, :integer, :null => false
+      column :user_id, :integer, :null => false
     end unless DB.table_exists?(:users_hostmasks) # }}}
   end
 
-  def self.down
-    [:bans,:channels,:console,:exemptions,:hostmasks,:ignores,:servers,:users,:users_hostmasks].each do |t|
+  def down
+    [:bans,:channels,:console,:exemptions,:hostmasks,:ignores,:levels,:servers,:users,:users_hostmasks].each do |t|
       drop_table t if DB.table_exists?(t)
     end
   end
